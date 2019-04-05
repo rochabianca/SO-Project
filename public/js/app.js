b1d43382b60f0be86b1f9210077374bdcf3f69c1
Bianca Rocha <biancarocha@MacBook-Air-de-Bianca.local>
2019-04-04 23:36:53 -0300

    changing state of dove

--- public/js/app.js
+++ public/js/app.js
@@ -9,10 +9,27 @@ function updateCaixaMensagens(numeroDeMensagens) {
 
 function mudancaEstadoPomboCorreio(estado) {
   // mudar a cor do pombo de acordo com os estados
+  var pombo = $('[data-target="kill-dove"]');
+  var estadoAnterior = pombo.data('state');
+  pombo.removeClass('badge-' + estadoAnterior);
+  pombo.data('state', estado);
+  pombo.addClass('badge-' + estado);
+
+  $('[data-target="console"]').prepend(
+    '<li class="list-group-item">O Pombo mudou seu estado para ' + estado + ' </li>'
+  );
 }
 
 function mudancaEstadoUsuario(id, estado) {
   // mudar a cor do usuario de acordo com os estados
+  var userId = '#' + id;
+  var previousState = $(userId).data('state');
+  $(userId).removeClass('badge-' + previousState);
+  $(userId).data('state', estado);
+  $(userId).addClass('badge-' + estado);
+  $('[data-target="console"]').prepend(
+    '<li class="list-group-item">O Usuário ' + id + ' mudou seu estado para ' + estado + ' </li>'
+  );
 }
 // cria o pombo
 $('[data-target="create-dove"]').on('click', function(e) {
@@ -21,7 +38,7 @@ $('[data-target="create-dove"]').on('click', function(e) {
   //numero mensagens, tempo carga, tempo voo, tempo descarga
   javaobj.criarPombo(10, 3, 10, 3);
   $('[data-target="dove"]').html(
-    '<span data-target="kill-dove" class="m-1 badge badge-info dove">' +
+    '<span data-target="kill-dove" data-state="info" class="m-1 p-2 badge badge-info dove">' +
       countDoves +
       ' <i class="fas fa-dove"></i></span>'
   );
@@ -42,13 +59,22 @@ $('[data-target="create-user"]').on('click', function(e) {
   var tempoEscrita = $('#tempo-escrita').val();
   var id;
   id = javaobj.criarUsuario(tempoEscrita);
-  console.log(id);
-  console.log('criou usuario');
-  $('[data-target="users"]').append(
-    '<span class="m-1 badge badge-primary">' + id + ' <i class="fas fa-user"></i></span>'
+  $('[data-add="users"]').append(
+    '<span id="' +
+      id +
+      '" data-toggle="kill-user" data-state="primary" class="m-1 p-2 badge badge-primary user">' +
+      id +
+      ' <i class="fas fa-user"></i></span>'
   );
 
   $('#CreateUser').modal('toggle');
+});
+
+$('[data-add="users"]').on('click', function(e) {
+  var id = '#' + e.target.id;
+  console.log(id);
+  $(id).remove();
+  killUser(e.target.id);
 });
 
 $('[data-target="create-inbox"]').on('click', function(e) {
@@ -61,14 +87,14 @@ $('[data-target="create-inbox"]').on('click', function(e) {
 function atualizarCaixaDeEntrada(numInbox) {
   var inboxes = $('[data-target="inboxes"]');
   inboxes.html(
-    '<span class="m-1 badge badge-dark">' + numInbox + ' <i class="fas fa-inbox"></i></span>'
+    '<span class="m-1 p-2 badge badge-dark">' + numInbox + ' <i class="fas fa-inbox"></i></span>'
   );
 }
 // verifica se existe ao menos um usuario e um pombo
 $(document).ready(function() {
   setInterval(function() {
     var dove = $('[data-target="dove"]').html().length;
-    var users = $('[data-target="users"]').html().length;
+    var users = $('[data-add="users"]').html().length;
     if (users > 0 && dove > 0) {
       $('[data-target="start"]').css({ display: 'block' });
     }
@@ -113,6 +139,8 @@ $('[data-target="start"]').click(function() {
 
 $('[data-target="stop"]').click(function() {
   $('[data-target="buttons"]').css({ display: 'block' });
+  $('[data-add="users"]').html('');
+  $('[data-target="dove"]').html('');
   $('[data-target="start"]').css({ display: 'block', visibility: 'visible' });
   $('[data-target="stop"]').css({ display: 'none', visibility: 'hidden' });
   javaobj.parar();
